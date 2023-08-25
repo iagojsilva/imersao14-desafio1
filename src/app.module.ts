4f5fed43504cce9f083dbe44a6766851e0e2ea2f
iagojsilva <iagosilva7contato@gmail.com>
2023-08-24 23:40:56 -0300

    Defining prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { RouterModule } from './router/router.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, RouterModule],
   controllers: [AppController],
   providers: [AppService],
 })
